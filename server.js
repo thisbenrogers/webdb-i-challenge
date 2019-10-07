ebd6c490002bfa665619529f8315ba3dbef3256e
Ben <thisbenrogers@gmail.com>
2019-10-07 18:34:59 -0500

    :sparkles: Adds endpoints
    Finishes Stretch

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const AccountRouter = require('./accounts/accounts-router');
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h2>Server running</h2>');
+});
 
 module.exports = server;
